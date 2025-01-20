4704ff3f2d5cd154953617379cb3b5347672ec2b
KID <50666207+KID421@users.noreply.github.com>
2025-01-20 21:40:55 +0800

    Update 認識 Switch 與列舉

--- Assets/Scripts/Class_5_1_Selection.cs
+++ Assets/Scripts/Class_5_1_Selection.cs
@@ -16,6 +16,10 @@ namespace KID
         private bool isOpen;
         [SerializeField, Header("分數"), Range(0, 100)]
         private int score = 100;
+        [SerializeField, Header("武器")]
+        private string weapon;
+        [SerializeField, Header("血量"), Range(0, 100)]
+        private int hp = 0;
 
         private void Awake()
         {
@@ -36,6 +40,10 @@ namespace KID
         // 更新事件：一秒鐘執行約 60 次 (60FPS) Frame Per Second
         private void Update()
         {
+            // 常用快捷鍵
+            // 1. 格式化 (排版) Ctrl + K D
+            // 2. 程式碼片段 Ctrl + K S 選 region
+            #region 判斷式 if
             Debug.Log("<color=#79f>更新事件</color>");
 
             // 如果 isOpen 布林值等於 True，就顯示已經開門
@@ -50,14 +58,103 @@ namespace KID
             }
 
             // 比較運算子、邏輯運算子結果為布林值
+            // if 會在最上方、else 在最下方，中間可以有多個 else if
+            // 如果 分數 >= 60 就通過
             if (score >= 60)
             {
                 Debug.Log("<color=#3f3>恭喜你的 C# 課程過了</color>");
             }
+            // 否則 如果 分數 >= 40 可以補考
+            else if (score >= 40)
+            {
+                Debug.Log("<color=#f96>你可以補考 C#</color>");
+            }
+            // 否則 如果 分數 >= 20 可以補考並且做一個小專題
+            else if (score >= 20)
+            {
+                Debug.Log("<color=#f9a>可以補考但是要做一個 C# 小專題</color>");
+            }
+            // 否則 當掉
             else
             {
                 Debug.Log("<color=#f33>你的 C# 課程被當了</color>");
             }
+            #endregion
+
+            #region 判斷式 switch
+            // switch 判斷式
+            // switch (要判斷的值) { 陳述式 }
+            // 快速完成：switch + Tab * 2
+            switch (weapon)
+            {
+                // case 值:
+                // 當判斷的值等於值時會執行這裡
+                // break; 跳出判斷式
+                // 如果武器等於小刀，攻擊力等於 20
+                // 蝴蝶刀會與小刀一樣
+                case "蝴蝶刀":
+                case "小刀":
+                    Debug.Log("<color=#ff3>攻擊力：20</color>");
+                    break;
+                case "美工刀":
+                    Debug.Log("<color=#ff3>攻擊力：10</color>");
+                    break;
+                case "屠龍刀":
+                    Debug.Log("<color=#ff3>攻擊力：999</color>");
+                    break;
+
+                // 當 weapon 的值不等於上面的所有值會執行這裡
+                default:
+                    Debug.Log("<color=#ff3>這是不能使用的武器</color>");
+                    break;
+            }
+            #endregion
+
+            #region 練習區
+            // 判斷式只會執行一個陳述式
+            // 錯誤寫法：順序
+            if (hp >= 10)
+            {
+                Debug.Log("<color=#f33>快死掉了</color>");
+            }
+            else if (hp >= 40)
+            {
+                Debug.Log("<color=#f33>警告，快喝水</color>");
+            }
+            else if (hp >= 60)
+            {
+                Debug.Log("<color=#f33>健康狀態有狀況</color>");
+            }
+            else if (hp >= 80)
+            {
+                Debug.Log("<color=#f33>血量安全</color>");
+            }
+            else if (hp == 0)
+            {
+                Debug.Log("<color=#f33>你已經死了</color>");
+            }
+            // 正確寫法 搭配邏輯運算子
+            if (hp >= 10 && hp < 40)
+            {
+                Debug.Log("<color=#3f3>快死掉了</color>");
+            }
+            else if (hp >= 40 && hp < 60)
+            {
+                Debug.Log("<color=#3f3>警告，快喝水</color>");
+            }
+            else if (hp >= 60 && hp < 80)
+            {
+                Debug.Log("<color=#3f3>健康狀態有狀況</color>");
+            }
+            else if (hp >= 80)
+            {
+                Debug.Log("<color=#3f3>血量安全</color>");
+            }
+            else if (hp == 0)
+            {
+                Debug.Log("<color=#3f3>你已經死了</color>");
+            } 
+            #endregion
         }
     }
 }
